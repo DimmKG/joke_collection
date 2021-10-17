4d477a8f85a61651bb0bc232fbe95ea4b3a1c47a
Dmitry Petrov <dimakrm361@gmail.com>
2021-10-17 14:29:46 +0700

    add benchmark

---
+++ src/benchmark.ts
@@ -0,0 +1,35 @@
+import { getJoke, getJokeAwait } from './app'
+
+const N = 20
+
+async function callAsync(N: number) {
+	const promises = []
+	for(let i = 0; i<N; i++) {
+		promises.push(getJoke())
+	}
+	await Promise.all(promises)
+	console.log('Async call complete')
+}
+
+async function callSync(N: number) {
+	for(let i = 0; i<N; i++) {
+		await getJokeAwait()
+	}
+	console.log('Sync call complete')
+}
+
+async function main() {
+	console.log('Testing sync call')
+	let start = performance.now()
+	await callSync(N)
+	let end = performance.now()
+	console.log('Execution time: ' + (end-start)+ ' ms')
+
+	console.log('Testing async call')
+	start = performance.now()
+	await callAsync(N)
+	end = performance.now()
+	console.log('Execution time: ' + (end-start) + ' ms')
+}
+
+main()
