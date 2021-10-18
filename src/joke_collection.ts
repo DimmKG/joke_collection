5a97ba6592fbf3630cf47c379605c18caabd954d
Dmitry Petrov <dimakrm361@gmail.com>
2021-10-18 11:58:07 +0700

    validate categories

--- src/joke_collection.ts
+++ src/joke_collection.ts
@@ -1,4 +1,5 @@
 import { Command } from 'commander'
+import { exit } from 'process'
 
 import { Error, getJoke } from './lib'
 
@@ -7,11 +8,25 @@ program.version('0.5')
 program.description('Get a random joke from Joke API "https://v2.jokeapi.dev/"')
 program.option('-c, --category [categories...]', 'specify categories. Case-insensitive' )
 program.parse(process.argv)
-
 const options = program.opts()
+
 let categories = 'Any'
-if (options.category) {
-	categories = options.category.join(',', '')
+const availableCats : Array<string> = ['programming', 'misc', 'dark', 'pun', 'spooky', 'christmas']
+
+if (options.category) {	
+	if(!Array.isArray(options.category)) {
+		console.log(`Available categories: ${availableCats.join(', ')}`)
+		exit(1)
+	}
+	const cats : Array<string> = options.category
+	cats.forEach((element) => {
+		if(availableCats.indexOf(element.toLowerCase()) === -1 ) {
+			console.log(`Invalid category: ${element}`)
+			console.log(`Available categories: ${availableCats.join(', ')}`)
+			exit(1)
+		}
+	})
+	categories = options.category.join(',')
 } 
 
 console.error('Getting a joke...\n')
